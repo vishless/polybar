2e8faaae1d1e3aa7d7242299f7470cdf27d4ef34
Vishless <mail.vishnupk@gmail.com>
2026-03-14 08:49:26 +0530

    Dynamically identify displays for output

--- launch.sh
+++ launch.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
 killall -q polybar
-sleep 1
+while pgrep -u $UID -x polybar >/dev/null; do sleep 0.1; done
 
-polybar primary &
-polybar secondary &
+# Get connected monitors
+MONITORS=($(xrandr --query | grep " connected" | cut -d" " -f1))
+COUNT=${#MONITORS[@]}
 
+export MONITOR_PRIMARY=${MONITORS[0]}
+
+if [ $COUNT -ge 2 ]; then
+    export MONITOR_SECONDARY=${MONITORS[1]}
+    MONITOR_PRIMARY=$MONITOR_PRIMARY polybar primary &
+    MONITOR_SECONDARY=$MONITOR_SECONDARY polybar secondary &
+else
+    MONITOR_PRIMARY=$MONITOR_PRIMARY polybar primary &
+fi
