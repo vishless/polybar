703a2347fc021ea7b2455d6f36d80f891f40483f
Vishless <mail.vishnupk@gmail.com>
2026-03-14 09:27:47 +0530

    Set primary monitor identified by xrander in i3

--- launch.sh
+++ launch.sh
@@ -3,16 +3,18 @@
 killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 0.1; done
 
-# Get connected monitors
-MONITORS=($(xrandr --query | grep " connected" | cut -d" " -f1))
-COUNT=${#MONITORS[@]}
+# Get primary monitor (set by xrandr --primary in i3 config)
+PRIMARY=$(xrandr --query | grep " connected primary" | cut -d" " -f1)
 
-export MONITOR_PRIMARY=${MONITORS[0]}
+# Get secondary monitor (any connected monitor that isn't primary)
+SECONDARY=$(xrandr --query | grep " connected" | grep -v "^$PRIMARY " | head -1 | cut -d" " -f1)
 
-if [ $COUNT -ge 2 ]; then
-    export MONITOR_SECONDARY=${MONITORS[1]}
-    MONITOR_PRIMARY=$MONITOR_PRIMARY polybar primary &
-    MONITOR_SECONDARY=$MONITOR_SECONDARY polybar secondary &
+export MONITOR_PRIMARY=$PRIMARY
+
+if [ -n "$SECONDARY" ]; then
+    export MONITOR_SECONDARY=$SECONDARY
+    MONITOR_PRIMARY=$PRIMARY polybar primary &
+    MONITOR_SECONDARY=$SECONDARY polybar secondary &
 else
-    MONITOR_PRIMARY=$MONITOR_PRIMARY polybar primary &
+    MONITOR_PRIMARY=$PRIMARY polybar primary &
 fi
