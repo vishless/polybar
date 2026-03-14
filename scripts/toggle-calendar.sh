504d90bd512c1e38ec8da815224878a8afa18757
Vishless <mail.vishnupk@gmail.com>
2026-03-14 10:12:45 +0530

    Read bar height from config instead of hardcoding

--- scripts/toggle-calendar.sh
+++ scripts/toggle-calendar.sh
@@ -3,21 +3,22 @@
 if pgrep -x yad > /dev/null; then
     pkill -x yad
 else
-    CAL_WIDTH=20
-    CAL_HEIGHT=176
-    BAR_HEIGHT=36
+    BAR_HEIGHT=$(awk '/^\[bar\/primary\]/{found=1} found && /^height/{print $3; exit}' ~/.config/polybar/config.ini)
 
     # Get primary monitor geometry
     GEOM=$(xrandr --query | grep " connected primary" | grep -oP '\d+x\d+\+\d+\+\d+')
     MON_W=$(echo $GEOM | cut -dx -f1)
     MON_X=$(echo $GEOM | grep -oP '(?<=\+)\d+' | sed -n '1p')
 
-    CAL_X=$((MON_X + MON_W - CAL_WIDTH))
+    CAL_PX_W=300
+    CAL_X=$((MON_X + MON_W - CAL_PX_W))
 
     yad --calendar \
         --undecorated \
         --fixed \
         --no-buttons \
         --close-on-unfocus \
-        --geometry=${CAL_WIDTH}x${CAL_HEIGHT}+${CAL_X}+${BAR_HEIGHT} &
+        --width=$CAL_PX_W \
+        --posx=$CAL_X \
+        --posy=$BAR_HEIGHT &
 fi
