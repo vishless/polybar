fb43254c6a7b9c36599d7e5a7c07673700114c03
Vishless <mail.vishnupk@gmail.com>
2026-04-19 09:24:07 +0530

    scripts/gpu.sh: pad gpu usage and add red warning color above 90%

--- scripts/gpu.sh
+++ scripts/gpu.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits | awk '{printf "%d%%", $1}'
+nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits | awk '{
+  pct = $1
+  if (pct > 90)
+    printf "%%{F#f38ba8}%2d%%%%{F-}", pct
+  else
+    printf "%2d%%", pct
+}'
